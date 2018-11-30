84a0f5b068b486c3dfd93d7eb772c56f21f8274e
Tommy Le <tommy.le2@gmail.com>
2018-11-30 17:18:39 -0500

    added login screen

--- firekit/AppDelegate.swift
+++ firekit/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         IQKeyboardManager.shared.enable = true
         
+        self.window?.rootViewController = LoginViewController()
+        
         return true
     }
 
---
+++ firekit/Login/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  SojuKit
+//
+//  Created by Tommy Le on 2018-11-30.
+//  Copyright © 2018 Tommy Le. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
