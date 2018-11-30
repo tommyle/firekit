9254d9e1ea88f5da5e1440932eed81a7ab6e3672
Tommy Le <tommy.le2@gmail.com>
2018-11-30 16:32:23 -0500

    First commit

---
+++ firekit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firekit
+//
+//  Created by Tommy Le on 2018-11-30.
+//  Copyright © 2018 Tommy Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ firekitTests/firekitTests.swift
@@ -0,0 +1,34 @@
+//
+//  firekitTests.swift
+//  firekitTests
+//
+//  Created by Tommy Le on 2018-11-30.
+//  Copyright © 2018 Tommy Le. All rights reserved.
+//
+
+import XCTest
+@testable import firekit
+
+class firekitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
